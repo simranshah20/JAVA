365b9eed25e519094fb60a3445234695aa601ae7
simran <shahsimran453@gmail.com>
2024-09-30 02:24:02 +0530

    Compound Ineterest

--- Challenge program code/src/10_CompoundInterest.java
+++ Challenge program code/src/10_CompoundInterest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class CompoundInterest {
     public static void main(String[] args) {
-        System.out.println("\n......Simple Interest.......\n");
+        System.out.println("\n......Compound Interest.......\n");
 
         Scanner sc = new Scanner(System.in);
         System.out.print("\nPlease enter your Principle amount Rs: ");
