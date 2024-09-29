d09aa64370ede8a1f3c05da63b97fdfef64e237f
simran <shahsimran453@gmail.com>
2024-09-30 01:59:48 +0530

    updated code for perimeter of rectangle

--- Challenge program code/src/07_PerimeterOfRectangle.java
+++ Challenge program code/src/07_PerimeterOfRectangle.java
@@ -5,9 +5,9 @@ class PerimeterOfRectangle {
         System.out.println("\n......Perimeter of rectangle.......\n");
 
         Scanner sc=new Scanner(System.in);
-        System.out.print("\nEnter length of perimeter in cm: ");
+        System.out.print("\nEnter length of rectangle in cm: ");
         double l=sc.nextDouble();
-        System.out.print("Enter width of perimeter in cm: ");
+        System.out.print("Enter width of rectangle in cm: ");
         double w=sc.nextDouble();
 
         double p=2*(l+w);
