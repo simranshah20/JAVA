393152af864bce68e7cd6bac0a2e7f581a1bb79b
simran <shahsimran453@gmail.com>
2024-09-30 02:16:34 +0530

    Simple Interest

--- Challenge program code/src/09_SimpleInterest.java
+++ Challenge program code/src/09_SimpleInterest.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 class SimpleInterest {
     public static void main(String[] args) {
-        sout
+        System.out.println("\n......Simple Interest.......\n");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("\nPlease enter your Principle amount Rs: ");
+        int p=sc.nextInt();
+        System.out.print("Now, tell me for how many years are you borrowing this money: ");
+        float t=sc.nextFloat();
+        System.out.print("Now, tell me your rate of Interest: ");
+        float r=sc.nextFloat();
+
+        float interest=(p*t*r)/100;
+        System.out.println("\nYour simple interest is RS: "+interest);
     }
 }
