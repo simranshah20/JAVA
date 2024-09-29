4ab3447fa820598ff146425a5421fc33383a0ca2
simran <shahsimran453@gmail.com>
2024-09-30 02:05:12 +0530

    Area of Triangle

--- Challenge program code/src/08_AreaOfTraingle.java
+++ Challenge program code/src/08_AreaOfTraingle.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class AreaOfTraingle {
+import java.util.Scanner;
+
+class AreaOfTraingle {
+    public static void main(String[] args) {
+        System.out.println("\n......Perimeter of rectangle.......\n");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("\nEnter Base of Triangle in cms: ");
+        double base=sc.nextDouble();
+        System.out.print("Enter perpendicular height of Triangle in cms: ");
+        double height=sc.nextDouble();
+
+        double area=(base*height)/2;
+        System.out.println("Area of Triangle in cms is: "+area+" cms2");
+
+    }
 }
