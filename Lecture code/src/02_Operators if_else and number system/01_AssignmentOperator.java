d40f7698ef45af1f0fdcd37807ff34f025e67973
simran <shahsimran453@gmail.com>
2024-09-30 00:15:53 +0530

    Assignment operator

---
+++ Lecture code/src/02_Operators if_else and number system/01_AssignmentOperator.java
@@ -0,0 +1,8 @@
+class AssignmentOperator{
+    public static void main(String[] args) {
+        int myInt=100;
+        System.out.println("Value is:"+myInt);
+        int myNum=myInt;  //assigning value of  myInt to myNum where = is called as assignmnet operator
+        System.out.println("Value after assignment operator: "+myNum);
+    }
+}
