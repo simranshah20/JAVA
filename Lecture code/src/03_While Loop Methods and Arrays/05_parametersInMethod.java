f617e1b36bdfc72fe89f0c3152891c73a04e3aa3
simran <shahsimran453@gmail.com>
2024-10-01 01:24:05 +0530

    parameters in method

---
+++ Lecture code/src/03_While Loop Methods and Arrays/05_parametersInMethod.java
@@ -0,0 +1,18 @@
+class parametersInMethod{
+    public static void main(String[] args) {
+    int num=sumTwoNumbers(4,5);   //here we are passing the values i.e 4 abd 5 which    are arguments
+        System.out.println("Sum of Two Numbers are: "+num);
+
+        //we can also write the above code like written below
+        System.out.println("\nAnother way of calling method:");
+        System.out.println("Sum of two number: "+sumTwoNumbers(8,4));
+    }
+
+    //we can return single value at once.
+    public static int sumTwoNumbers(int a,int b){   //here we are declaring a nd b which are parameters
+        System.out.println("First Number received is: "+a);
+        System.out.println("Second Number received is: "+b);
+        int sum =a+b;
+        return sum;
+    }
+}
