c2ff2dcd79c7bde3f4f5c454169dd55f1d51d416
simran <shahsimran453@gmail.com>
2024-09-30 20:57:04 +0530

    Check number is positive,negative or zero

---
+++ Challenge program code/src/12_ChecksNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class ChecksNumber {
+    public static void main(String[] args) {
+        System.out.println("\n.........Checks whether the number is positive,negative or Zero...........\n");
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int num=input.nextInt();
+
+        if(num>0){
+            System.out.println(num+" is positive number.");
+        }else if(num==0){
+            System.out.println(num+" is zero.");
+        }else{
+            System.out.println(num+" is negative number.");
+        }
+    }
+}
