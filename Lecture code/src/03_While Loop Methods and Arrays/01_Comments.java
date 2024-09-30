9747ea9b4593868d059d3a66f72c00bd71374dec
simran <shahsimran453@gmail.com>
2024-09-30 23:11:23 +0530

    Java Comments

---
+++ Lecture code/src/03_While Loop Methods and Arrays/01_Comments.java
@@ -0,0 +1,18 @@
+class Comments{
+    /**
+     * this is used for java docs
+     * this is my main method
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("\n..........Comments............\n");
+
+        //this is single line comment
+        System.out.println("Hello Everyone,How are you?");
+
+        /*
+        This is multi line comment
+        this is the  2nd line of comment
+         */
+    }
+}
