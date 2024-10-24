de1e93dae554432e5d03f42fed76300045f89ea7
simran <shahsimran453@gmail.com>
2024-10-25 02:52:13 +0530

    Reverse of an array using method

--- Challenge program code/src/38_ReverseArray.java
+++ Challenge program code/src/38_ReverseArray.java
@@ -1,22 +1,42 @@
 class ReverseArray {
     public static void main(String[] args) {
+        // Printing a title message to introduce the purpose of the program
         System.out.println("\n...............Reverse of an Array....................\n");
 
-        int arr[] = ArrayUtility.inputArray();   //takes input from user
+        // Calling a method from ArrayUtility class to take user input and store it in the 'arr' array
+        int arr[] = ArrayUtility.inputArray();
 
-        reverse(arr);     //method call
+        // Calling the 'reverse' method to reverse the contents of the 'arr' array
+        reverse(arr);
+
+        // Printing the reversed array
         System.out.println("Your Reversed Array is: ");
+
+        // Calling a method from ArrayUtility class to display the contents of the array
         ArrayUtility.displayArray(arr);
 
     }
-    public static void reverse(int numArr[]){
+
+    // Method to reverse the elements of the input array 'numArr'
+    public static void reverse(int numArr[]) {
+        // 'i' is initialized to 0 and used to track the index from the start of the array
         int i = 0;
-        while(i < numArr.length/2){
+
+        // The loop runs until the middle of the array is reached (numArr.length/2)
+        while (i < numArr.length / 2) {
+            // Storing the current element from the front of the array in 'swap'
             int swap = numArr[i];
-            numArr[i] = numArr[(numArr.length - 1)-i];
-            numArr[(numArr.length - 1)-i] = swap;
+
+            // Replacing the current front element with the corresponding element from the end of the array
+            numArr[i] = numArr[(numArr.length - 1) - i];
+
+            // Replacing the element from the end of the array with the stored value in 'swap'
+            numArr[(numArr.length - 1) - i] = swap;
+
+            // Incrementing 'i' to move to the next index from the front
             i++;
         }
+        // Return statement is not necessary here, since the method returns nothing (void)
         return;
     }
 }
