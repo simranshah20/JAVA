8430210cbd5d3acc52622850ae23d0e6c0ca0415
simran <shahsimran453@gmail.com>
2024-11-09 22:33:44 +0530

    Week Detector using Switch Case

--- Lecture code/src/08_Control statement math and string/02_Switch.java
+++ Lecture code/src/08_Control statement math and string/02_Switch.java
@@ -1,40 +1,50 @@
 import java.util.Scanner;
 
-class Switch{
+class Switch {
     public static void main(String[] args) {
+        // Create a Scanner object to read user input
         Scanner input = new Scanner(System.in);
+
+        // Display a welcome message
         System.out.println("Welcome to day of the week detector\n");
+
+        // Prompt the user to enter a day as a number
         System.out.print("Enter your day in number: ");
         int day = input.nextInt();
 
-        switch (day){
+        // Use a switch statement to determine the day of the week
+        switch (day) {
             case 1:
                 System.out.println("Sunday");
-                break;
+                break;  // Exit switch after printing Sunday
             case 2:
                 System.out.println("Monday");
-                break;
+                break;  // Exit switch after printing Monday
             case 3:
                 System.out.println("Tuesday");
-                break;
+                break;  // Exit switch after printing Tuesday
             case 4:
                 System.out.println("Wednesday");
-                break;
+                break;  // Exit switch after printing Wednesday
             case 5:
                 System.out.println("Thursday");
-                break;
+                break;  // Exit switch after printing Thursday
             case 6:
                 System.out.println("Friday");
-                break;
+                break;  // Exit switch after printing Friday
             case 7:
                 System.out.println("Saturday");
-                break;
-//            case 8:   //this is called as fall through where we do not write break
-//            case 9:
-//                System.out.println("Holiday");
+                break;  // Exit switch after printing Saturday
+
+            // Uncomment the following lines for a fall-through example:
+            // Fall-through: cases 8 and 9 will both print "Holiday" without a break statement
+            // case 8:
+            // case 9:
+            //     System.out.println("Holiday");
+
             default:
-                System.out.println("Invalid Day");
-                break;
+                System.out.println("Invalid Day");  // If no cases match, this line executes
+                break;  // Break is optional here since it's the last statement in the switch
         }
     }
 }
