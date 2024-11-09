8fd661de29b89c13958d9ddac158a0ec723bc768
simran <shahsimran453@gmail.com>
2024-11-09 23:09:07 +0530

    Check student score using ternary operator

--- Challenge program code/src/49_StudentScore.java
+++ Challenge program code/src/49_StudentScore.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class StudentScore {
+import java.util.Scanner;
+
+class StudentScore {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Prompt the user to enter a number
+        System.out.print("Enter your marks: ");
+        int marks = input.nextInt();
+        String result = marks > 80 ? "High" : (marks > 50 ? "Moderate" : "Low");
+        System.out.println("Your score is: "+result);
+    }
 }
