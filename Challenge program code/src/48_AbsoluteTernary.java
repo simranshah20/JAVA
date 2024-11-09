12d0f338deae05d2959f0f995f6031dd546454e0
simran <shahsimran453@gmail.com>
2024-11-09 23:03:28 +0530

    Calculate absolute value using ternary operator

--- Challenge program code/src/48_AbsoluteTernary.java
+++ Challenge program code/src/48_AbsoluteTernary.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 
 class AbsoluteTernary {
     public static void main(String[] args) {
+        // Create a Scanner object to read user input
         Scanner input = new Scanner(System.in);
 
         // Prompt the user to enter a number
         System.out.print("Enter your number: ");
         int num = input.nextInt();
+
+        // Use a ternary operator to calculate the absolute value
+        // If num is greater than or equal to 0, result is num; otherwise, result is -num
         int result = num >= 0 ? num : -num;
-        System.out.println("Absolute value is: "+result);
+
+        // Display the absolute value
+        System.out.println("Absolute value is: " + result);
     }
 }
