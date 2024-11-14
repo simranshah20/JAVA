40f8a8d2f645a22e456458334798eea136c66c87
simran <shahsimran453@gmail.com>
2024-11-15 00:05:14 +0530

    Getter Setter

---
+++ Lecture code/src/09_Encapsulation and Inheritance/01_GetterSetter.java
@@ -0,0 +1,19 @@
+public class Cars {
+    private String color,model;
+    private double fuelLevel;
+    private long costOfPurchage;
+
+    public Cars(String color, String model, double fuelLevel, long costOfPurchage) {
+        this.color = color;
+        this.model = model;
+        this.fuelLevel = fuelLevel;
+        this.costOfPurchage = costOfPurchage;
+    }
+
+    String getColor(){
+        return color;
+    }
+    public String getModel(){
+        return model;
+    }
+}
