3d1e623ff85d557f49bfcb952db5b9b47d732f5b
simran <shahsimran453@gmail.com>
2024-11-14 23:06:24 +0530

    Packages

---
+++ Lecture code/src/in/kgcoding/01_Packages.java
@@ -0,0 +1,13 @@
+package in.kgcoding; // Declares the package for this file as in.kgcoding
+
+import java.util.Scanner; // Imports the Scanner class from the java.util package for user input
+
+class Packages {
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner input = new Scanner(System.in);
+
+        // Creates an instance of the Packages class from the in.knowledgegate package
+        in.knowledgegate.Packages packages = new in.knowledgegate.Packages();
+    }
+}
